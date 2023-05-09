7996b2adbb41154724480aa133dd31c05688f6e8
Anselem Odimegwu <odimegwu@systemspecs.com.ng>
2023-05-09 16:29:33 +0100

    feat: handle event sync between services

--- event-bus/app.js
+++ event-bus/app.js
@@ -5,18 +5,28 @@ const app = express();
 
 app.use(express.json());
 
+const events = []
+
 app.post('/events', async (req, res) => {
     console.log('Received Event', req.body.type);
+
+    const event = req.body;
+    events.push(event);
+    console.log('All events saved')
     try {
-        await axios.post('http://localhost:4000/api/v1/events', req.body)
-        await axios.post('http://localhost:4001/api/v1/events', req.body)
-        await axios.post('http://localhost:4002/api/v1/events', req.body)
-        await axios.post('http://localhost:4003/api/v1/events', req.body)
+        await axios.post('http://localhost:4000/api/v1/events', req.body).catch(err => { console.log(err)})
+        await axios.post('http://localhost:4001/api/v1/events', req.body).catch(err => { console.log(err)})
+        await axios.post('http://localhost:4002/api/v1/events', req.body).catch(err => { console.log(err)})
+        await axios.post('http://localhost:4003/api/v1/events', req.body).catch(err => { console.log(err)})
 
         res.status(200).json({ message: "Event sent to all services"});
     } catch(err) {
         console.log(err)
     }
+})
+
+app.get('/events', (req, res) => {
+    res.status(200).json({ message: 'All events returned', events })
 })
 
 app.listen('4005', () => {
--- query/app.js
+++ query/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const axios = require('axios')
 
 const app = express();
 app.use(cors());
@@ -7,6 +8,34 @@ app.use(cors());
 app.use(express.json());
 
 const posts = []
+
+const handleEvents = (type, data) => {
+    if(type === 'PostCreated') {
+        const { id, title } = data;
+        posts.push({ id, title, comments: []})
+    }
+    if(type === 'CommentCreated') {
+        const { commentId, comment, postId, status } = data;
+        posts.forEach(item => {
+            if(item.id === postId) {
+                item.comments.push({ commentId, comment, status })
+            }
+        })
+    }
+    if(type === 'CommentUpdated') {
+        const { commentId, comment, postId, status } = data;
+        posts.forEach(item => {
+            if(item.id === postId) {
+                item.comments.forEach(commentItem => {
+                    if(commentItem.commentId === commentId) {
+                        commentItem.status = status
+                        commentItem.comment = comment
+                    }
+                })
+            }
+        })
+    }
+}
 
 app.get('/api/v1/posts', (req, res) => {
     res.status(200).json({ message: 'All posts returned', posts})
@@ -16,38 +45,20 @@ app.post('/api/v1/events', (req, res) => {
     console.log('Received Event', req.body.type)
     const { type, data } = req.body;
     try {
-
-        if(type === 'PostCreated') {
-            const { id, title } = data;
-            posts.push({ id, title, comments: []})
-        }
-        if(type === 'CommentCreated') {
-            const { commentId, comment, postId, status } = data;
-            posts.forEach(item => {
-                if(item.id === postId) {
-                    item.comments.push({ commentId, comment, status })
-                }
-            })
-        }
-        if(type === 'CommentUpdated') {
-            const { commentId, comment, postId, status } = data;
-            posts.forEach(item => {
-                if(item.id === postId) {
-                    item.comments.forEach(commentItem => {
-                        if(commentItem.commentId === commentId) {
-                            commentItem.status = status
-                            commentItem.comment = comment
-                        }
-                    })
-                }
-            })
-        }
+        handleEvents(type, data)
         res.status(200).json({ message: 'Event received', posts })
     } catch(err) {
         console.log(err)
     }
 })
 
-app.listen('4002', () => {
+app.listen('4002', async () => {
     console.log('Server listening on port 4002')
+
+    const { data } = await axios.get('http://localhost:4005/events')
+    data.events.forEach(event => {
+        console.log('Processing event:', event.type)
+        handleEvents(event.type, event.data)
+    })
+
 })
