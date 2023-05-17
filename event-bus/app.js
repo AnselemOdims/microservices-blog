503d6e15b0dd972700be3970ffe56b793249e9e9
Anselem Odimegwu <odimegwu@systemspecs.com.ng>
2023-05-17 12:30:13 +0100

    Change services clusterip in event-bus

--- event-bus/app.js
+++ event-bus/app.js
@@ -15,9 +15,9 @@ app.post('/events', async (req, res) => {
     console.log('All events saved')
     try {
         await axios.post('http://posts-clusterip-srv:4000/api/v1/events', req.body).catch(err => { console.log(err)})
-        // await axios.post('http://localhost:4001/api/v1/events', req.body).catch(err => { console.log(err)})
-        // await axios.post('http://localhost:4002/api/v1/events', req.body).catch(err => { console.log(err)})
-        // await axios.post('http://localhost:4003/api/v1/events', req.body).catch(err => { console.log(err)})
+        await axios.post('http://comments-clusterip-srv:4001/api/v1/events', req.body).catch(err => { console.log(err)})
+        await axios.post('http://query-clusterip-srv:4002/api/v1/events', req.body).catch(err => { console.log(err)})
+        await axios.post('http://moderation-clusterip-srv:4003/api/v1/events', req.body).catch(err => { console.log(err)})
 
         res.status(200).json({ message: "Event sent to all services"});
     } catch(err) {
