6053af0c89d2ee4d298846fbef8dbf03d2831605
Anselem Odimegwu <odimegwu@systemspecs.com.ng>
2023-05-23 10:19:40 +0100

    Change the endpoint to get all comments in the frontend

--- frontend/src/CommentCreate.js
+++ frontend/src/CommentCreate.js
@@ -7,7 +7,7 @@ const CommentCreate = ({ postId }) => {
   const onSubmit = async (event) => {
     event.preventDefault();
 
-    await axios.post(`http://localhost:4001/api/v1/posts/${postId}/comments`, {
+    await axios.post(`http://posts.com/api/v1/posts/${postId}/comments`, {
       comment,
     });
 
